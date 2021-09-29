8bcc497eb069ae1d8edcc30e1058838fd15de27c
Eric Chiang <ericchiang@google.com>
2021-09-28 20:19:43 -0700

    *: add LICENSE information

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
 
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 wget http://docs.oasis-open.org/pkcs11/pkcs11-base/v2.40/errata01/os/include/pkcs11-v2.40/pkcs11.h -O pkcs11/pkcs11.h
 wget http://docs.oasis-open.org/pkcs11/pkcs11-base/v2.40/errata01/os/include/pkcs11-v2.40/pkcs11t.h -O pkcs11/pkcs11t.h
 wget http://docs.oasis-open.org/pkcs11/pkcs11-base/v2.40/errata01/os/include/pkcs11-v2.40/pkcs11f.h -O pkcs11/pkcs11f.h
